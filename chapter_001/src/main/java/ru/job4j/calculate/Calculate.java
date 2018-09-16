e4c618ab0aa781ca7ec4b071c5c11c4208bcd11c
MaratYanbaev <yanbaevms@gmail.com>
2018-09-17 00:48:26 +0400

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+ * Class Calculate решение задачи части 001 урок 1. 
+ * @author Marat Yanbaev (yanbaevms@gmail.com)
+ * @since 16.09.2018
+ */
 public class Calculate{
 
-	public static void main (String [] args){
-		
+	/**
+	 * Конструктор, вывод строки в консоль.
+	 * @param args - массив строковых аргументов.
+	 */
+	public static void main (String [] args) {
 		System.out.println("Hello world!");
 	}
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+//*
+*Package for calculate task
+*
+*@author Marat Yanbaev (yanbaevms@gmail.com)
+*@version $id$
+*@since 0.1
+*/
+package ru.job4j.calculate;
