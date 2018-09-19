01c2982ee977192b0ca3875cc4997016a25d309a
MaratYanbaev <yanbaevms@gmail.com>
2018-09-19 14:32:49 +0400

    1.5 Checkstyle[#71966]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -5,13 +5,22 @@ package ru.job4j.calculate;
  * @author Marat Yanbaev  (yanbaevms@gmail.com)
  * @since 16.09.2018
  */
-public class Calculate{
+public class Calculate {
 
 	/**
-	 * Конструктор, вывод строки в консоль.
+	 * Main.
 	 * @param args - массив строковых аргументов.
 	 */
-	public static void main (String [] args) {
+	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
+	
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	} 
 }
