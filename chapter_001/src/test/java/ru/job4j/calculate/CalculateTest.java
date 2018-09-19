e260e04dc0d1b3ec1c63597c4b387975e40fde5a
MaratYanbaev <yanbaevms@gmail.com>
2018-09-19 13:32:03 +0400

    1.4. JUnit [#4122]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+	/**
+	* Test.
+	*
+	* @author Marat Yanbaev (yanbaevms@gmail.com)
+	* @version $Id$
+	* @since 0.1
+	*/
+	public class CalculateTest {
+		/**
+		* Test echo.
+		*/
+		@Test
+		public void whenTakeNameThenTreeEchoPlusName() {
+			String input = "Marat Yanbaev";
+			String expect = "Echo, echo, echo : Marat Yanbaev"; 
+			Calculate calc = new Calculate();
+			String result = calc.echo(input);
+			assertThat(result, is(expect));
+		}
+ 
+	}
