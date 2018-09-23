6e465b7f16c4d5fec2b26971ba3718b228b3a7f3
MaratYanbaev <yanbaevms@gmail.com>
2018-09-24 00:26:09 +0400

    4.3. Вычисление площади треугольника [#9461]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -21,7 +21,7 @@ public class Triangle {
      * @param ab расстояние между точками a b
      * @param ac расстояние между точками a c
      * @param bc расстояние между точками b c
-     * @return Периметр.
+     * @return Полупериметр треугольника.
      */
 
     public double period(double ab, double ac, double bc) {
@@ -41,7 +41,7 @@ public class Triangle {
         double ac = this.a.distanceTo(this.c);
         double bc = this.b.distanceTo(this.c);
         double p = this.period(ab, ac, bc);
-        if (this.exist(ab, ac, bc)) {
+        if (this.exist(ab, ac, bc, p)) {
             rsl = Math.sqrt(((p - ab) * (p - ac) * (p - bc)) * p);
         }
         return rsl;
@@ -53,9 +53,10 @@ public class Triangle {
      * @param ab Длина от точки a b.
      * @param ac Длина от точки a c.
      * @param bc Длина от точки b c.
+     * @param p Полупериметр треугольника.
      * @return правду или ложь
      */
-    private boolean exist(double ab, double ac, double bc) {
-        return (ab + ac != bc) & (ab + bc != ac) & (ac + bc != ab);
+    private boolean exist(double ab, double ac, double bc, double p) {
+        return ((ab + ac != bc) & (ab + bc != ac) & (ac + bc != ab) & (p > ab) & (p > ac) & (p > bc));
     }
 }
