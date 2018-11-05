7ca2e245331480cd38606e458bb5ba0511c1e851
MaratYanbaev <yanbaevms@gmail.com>
2018-11-05 16:22:30 +0400

    3. if-else-throw [#49895]

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -7,7 +7,23 @@ public class ConsoleInput implements Input {
     private Scanner scanner = new Scanner(System.in);
 
     public String ask(String question) {
-        System.out.println(question);
+        System.out.print(question + "\n");
         return scanner.nextLine();
+    }
+
+    @Override
+    public int ask(String question, int[] ranges) {
+        int key = Integer.parseInt(this.ask(question));
+        boolean exist = false;
+        for (int value : ranges) {
+            if (value == key) {
+                exist = true;
+                break;
+            }
+        }
+        if (!exist) {
+            throw new MenuOutException("Пожайлуста введите номер из дипазона пунктов.\n\n");
+        }
+        return key;
     }
 }
