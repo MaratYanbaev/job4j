fcc874de8e5d6fc30cd11ecd57ff70b2b3c39f95
MaratYanbaev <yanbaevms@gmail.com>
2019-01-25 16:37:00 +0400

    5.3.4. Задан связанный список. Определить цикличность.[#105795]

--- chapter_004/src/main/java/ru/job4j/list/NodeLoop.java
+++ chapter_004/src/main/java/ru/job4j/list/NodeLoop.java
@@ -12,21 +12,27 @@ public class NodeLoop {
      * @return - if list has hasCycle true otherwise false
      */
     public boolean hasCycle(Node first) {
-        boolean result = false;
         Node fast = first;
         Node slow = first;
-        while (fast.next != first && fast.next != null && !result) {
-            for (int i = 2; i > 0; i--) {
-                fast = fast.next;
-                if (fast.next == first) {
-                    result = true;
-                } else if (fast == slow || fast.next == null) {
-                    return result;
-                }
-
-            }
+//        while (fast.next != first && fast.next != null && !result) {
+//            for (int i = 2; i > 0; i--) {
+//                fast = fast.next;
+//                if (fast.next == first) {
+//                    result = true;
+//                } else if (fast == slow || fast.next == null) {
+//                    return result;
+//                }
+//
+//            }
+//            slow = slow.next;
+//        }
+        while (fast.next != null && fast.next.next != null && fast.next != first) {
+            fast = fast.next.next;
             slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
         }
-        return result;
+        return fast.next == first;
     }
 }
