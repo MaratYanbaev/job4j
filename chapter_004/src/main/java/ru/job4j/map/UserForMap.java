e619443bcbf1ca85ed9d96d0baf7823639bd3b66
MaratYanbaev <yanbaevms@gmail.com>
2019-01-25 21:39:49 +0400

    1. Реализовать коллекцию Set на массиве[#105808]

---
+++ chapter_004/src/main/java/ru/job4j/map/UserForMap.java
@@ -0,0 +1,26 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * 1. Создать модель User[#105811]
+ *
+ * Создать модель User и три поля
+ * String name,
+ * int children,
+ * Calendar birthday,
+ *
+ * @author Marat Yanbaev (yanbaevms@gmail.com)
+ * @since 25.01.2019
+ */
+public class UserForMap {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+
+    public UserForMap(String name, int children) {
+        this.name = name;
+        this.children = children;
+    }
+}
